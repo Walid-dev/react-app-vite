5ed5e9a960d44fec14aac99b48de11d58865be95
Walid <walid.hlali@mobi2go.com>
2023-03-04 21:31:51 -0500

    Item display finalized

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
 import { ItemReducer, ActionTypes, INITIAL_STATE } from "./ItemReducer";
+import { v4 as uuidV4 } from "uuid";
 import "./item.css";
 
 export const Item = () => {
@@ -13,74 +14,76 @@ export const Item = () => {
       .catch((err) => dispatch({ type: ActionTypes.FETCH_ERROR, payload: [] }));
   };
 
+  if (state.post.data) {
+    console.log("Exists");
+    console.log(state.post.data);
+  }
+
   return (
-    <div className="item-container">
-      <div className="item">
-        <div className="name-container">
-          <div className="name"></div>
-        </div>
-        <div className="image-container">
-          <div className="image">
-            <img src="https://picsum.photos/278/200" alt="random" />
-          </div>
-        </div>
-        <div className="main-content-container">
-          <div className="description-container">
-            <div className="item-description">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.
-            </div>
-          </div>
-          <div className="tags-container">
-            <small className="tags">summer</small>
-            <small className="tags">cotton</small>
-            <small className="tags">streetwear</small>
-            <small className="tags">organic</small>
-            <small className="tags">soft</small>
-          </div>
-          <div className="color-icons-container">
-            <button className="color-icon red"></button>
-            <button className="color-icon yellow"></button>
-            <button className="color-icon green"></button>
-            <button className="color-icon black"></button>
-            <button className="color-icon white"></button>
-            <button className="color-icon purple"></button>
-          </div>
-          <div className="size-icons-container">
-            <div className="size-icon-box">
-              <button className="size-icon">XS</button>
-            </div>
-            <div className="size-icon-box">
-              <button className="size-icon">S</button>
-            </div>
-            <div className="size-icon-box">
-              <button className="size-icon">M</button>
-            </div>
-            <div className="size-icon-box">
-              <button className="size-icon">L</button>
-            </div>
-            <div className="size-icon-box">
-              <button className="size-icon">XL</button>
-            </div>
-            <div className="size-icon-box">
-              <button className="size-icon">XXL</button>
-            </div>
-          </div>
-          <div className="quantity-container">
-            <div className="quantity">
-              <button type="button">-</button>
-              <div>Quantity</div>
-              <span>0</span>
-              <button type="button">+</button>
-            </div>
-          </div>
-          <div className="prices-container">
-            <div className="price">
-              <span className="currency">$</span>9.99
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
       <button onClick={handleFetch}>Get Data</button>
+      {state.post.data?.map((item: any) => (
+        <div key={uuidV4()} className="item-container">
+          <div className="item">
+            <div className="name-container">
+              <div className="name">{item.name}</div>
+            </div>
+            <div className="image-container">
+              <div className="image">
+                <img src={item.images.sm.one} alt={item.name} />
+              </div>
+            </div>
+            <div className="main-content-container">
+              <div className="description-container">
+                <div className="item-description">{item.desc}</div>
+              </div>
+              <div className="tags-container">
+                {item.tags.map((tag: string) => {
+                  return (
+                    <small key={uuidV4()} className="tags">
+                      {tag}
+                    </small>
+                  );
+                })}
+              </div>
+              <div className="color-icons-container">
+                {item.colors.map((color: string) => {
+                  return <span className={`color-icon ${color}`}></span>;
+                })}
+              </div>
+              {/* <button className="color-icon yellow"></button>
+                <button className="color-icon green"></button>
+                <button className="color-icon black"></button>
+                <button className="color-icon white"></button>
+                <button className="color-icon purple"></button>
+              </div> */}
+              <div className="size-icons-container">
+                {item.sizes.map((size: string) => {
+                  return (
+                    <div key={uuidV4()} className="size-icon-box">
+                      <button className="size-icon">{size}</button>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="quantity-container">
+                <div className="quantity">
+                  <button type="button">-</button>
+                  <div>Quantity</div>
+                  <span>{item.quantity}</span>
+                  <button type="button">+</button>
+                </div>
+              </div>
+              <div className="prices-container">
+                <div className="price">
+                  <span className="currency">$</span>
+                  {item.price}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
